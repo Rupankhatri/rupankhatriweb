0d52f88f21f4ef6c1aa9308cbd6d1c54ac864d07
Rupankhatri <141803588+Rupankhatri@users.noreply.github.com>
2023-09-13 11:26:09 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function () {
+
+});
+
+window.onscroll = () => {
+    navbar.classlist.remove("active");
+};
